2bb32c6a7e7478f01578d046670150228fd5279b
TaskerJang <tasker.dev103@gmail.com>
2026-04-03 10:41:46 +0900

    refactor(parser): 구 parser/ 디렉토리 삭제

--- parser/__init__.py
+++
@@ -1,5 +0,0 @@
-# ⚠️  이 디렉토리는 doc_parser/로 이전되었습니다.
-# 로컬에서 아래 명령어로 삭제하세요:
-#   git rm -r parser/
-#   git commit -m "refactor(parser): 구 parser/ 디렉토리 삭제"
-raise ImportError("parser 패키지는 doc_parser로 이전되었습니다. import doc_parser를 사용하세요.")
